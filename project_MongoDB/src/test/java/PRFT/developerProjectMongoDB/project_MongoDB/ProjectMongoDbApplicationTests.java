2a6f6b2c5899137aab813f0dbd1f6731115408ec
siddarth.gumireddy <gsreddy@bu.edu>
2022-07-11 13:39:39 -0400

    first commit

---
+++ project_MongoDB/src/test/java/PRFT/developerProjectMongoDB/project_MongoDB/ProjectMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package PRFT.developerProjectMongoDB.project_MongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

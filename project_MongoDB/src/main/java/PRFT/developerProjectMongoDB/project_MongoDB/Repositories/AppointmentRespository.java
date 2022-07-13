d7c983f57c168a6f9522c897cf93243f8144101b
siddarth.gumireddy <gsreddy@bu.edu>
2022-07-13 01:28:52 -0400

    Add, List, Get, Delete, Update

--- project_MongoDB/src/main/java/PRFT/developerProjectMongoDB/project_MongoDB/Repositories/AppointmentRespository.java
+++ project_MongoDB/src/main/java/PRFT/developerProjectMongoDB/project_MongoDB/Repositories/AppointmentRespository.java
@@ -4,10 +4,48 @@ import PRFT.developerProjectMongoDB.project_MongoDB.model.Appointment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 
 public interface AppointmentRespository extends MongoRepository<Appointment, UUID> {
+
+
+    default Appointment findByApptID(UUID id){
+        List<Appointment> AllAppointments= this.findAll();
+        for (int i = 0; i < AllAppointments.size(); i++){
+            Appointment newOne = AllAppointments.get(i);
+            if (newOne.getAppointmentID().equals(id)){
+                return newOne;
+
+            }
+        }
+        return AllAppointments.get(0); //change this to none
+    }
+
+    default void deleteByApptId(UUID id){
+        List<Appointment> AllAppointments= this.findAll();
+        for (int i = 0; i < AllAppointments.size(); i++){
+            Appointment newOne = AllAppointments.get(i);
+            if (newOne.getAppointmentID().equals(id)){
+                Appointment newOne2 = newOne;
+                this.delete(newOne2);
+            }
+        }
+    }
+
+    default List<Appointment> getAllExcept(UUID apptID) {
+        int index2 = 0;
+        List<Appointment> AllAppointments= this.findAll();
+        for (int i = 0; i < AllAppointments.size(); i++){
+            Appointment newOne = AllAppointments.get(i);
+            if (newOne.getAppointmentID().equals(apptID)){
+                index2 = i;
+            }
+        }
+        AllAppointments.remove(index2);
+        return AllAppointments;
+    }
 
 
 }
--- project_MongoDB/src/main/java/PRFT/developerProjectMongoDB/project_MongoDB/controller/AppointmentController.java
+++ project_MongoDB/src/main/java/PRFT/developerProjectMongoDB/project_MongoDB/controller/AppointmentController.java
@@ -2,6 +2,7 @@ package PRFT.developerProjectMongoDB.project_MongoDB.controller;
 
 import PRFT.developerProjectMongoDB.project_MongoDB.Repositories.AppointmentRespository;
 import PRFT.developerProjectMongoDB.project_MongoDB.model.Appointment;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 @RequestMapping("/api/v1/appointment")
@@ -24,14 +28,31 @@ public class AppointmentController {
         return ResponseEntity.ok(save);
     }
 
-    @GetMapping("/GetAll/") //List All Appointments
-    public ResponseEntity<?> getAllAppointments(){
-    return ResponseEntity.ok(this.appointmentRespository.findAll());
+    @GetMapping("/List/") //List All Appointments
+    public ResponseEntity<?> getAllAppointments() {
+        return ResponseEntity.ok(this.appointmentRespository.findAll());
     }
 
-    @GetMapping("/GetOne/") //Get one Appointment by ID
-    public ResponseEntity<?> getAppointment(@RequestBody String apptID){
-        UUID apptID2 = UUID.fromString(apptID);
-        return ResponseEntity.ok(this.appointmentRespository.findById(apptID2));
+    @GetMapping({"/GetOne/{apptID}"}) //Get one Appointment by ID
+    public ResponseEntity<?> getAppointment(@PathVariable("apptID") UUID apptID) {
+        return ResponseEntity.ok(this.appointmentRespository.findByApptID(apptID));
+    }
+
+    @DeleteMapping({"/DeleteOne/{apptID}"}) //Delete one Appointment by ID
+    public String deleteAppointment(@PathVariable("apptID") UUID apptID) {
+        List<Appointment> newAppt = this.appointmentRespository.getAllExcept(apptID);
+        appointmentRespository.deleteAll();
+        for(int i = 0; i < newAppt.size(); i++){
+            createAppointment(newAppt.get(i));
+        }
+
+        return "Appointment with appointment ID: has been deleted successfully.";
+    }
+
+    @PutMapping("/UpdateAppointment/{apptID}")
+    public ResponseEntity<?> updateAppointment(@RequestBody Appointment appointment, UUID apptID) {
+        appointmentRespository.deleteById((apptID));
+        Appointment save = this.appointmentRespository.save(appointment);
+        return ResponseEntity.ok(save);
     }
 }
